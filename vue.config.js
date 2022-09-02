4db4b0044a329106c661daa5e5e9e10ff330258c
Rhuan Felipe <rhuanfelipe@MacBook-Pro-de-Rhuan.local>
2022-09-01 21:31:58 -0300

    feat: preparing for gh-actions

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/sandbox-test/" : "/",
   };
   
